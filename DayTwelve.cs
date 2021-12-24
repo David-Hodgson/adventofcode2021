6874390702ff50e83e036ce75804d651eecb96a6
David Hodgson <dahodgson@gmail.com>
2021-12-24 20:38:38 +0000

    Day 12 - Part Two Complete

--- DayTwelve.cs
+++ DayTwelve.cs
@@ -10,7 +10,8 @@ namespace aoc2021
 
 			Dictionary<string,List<string>> graph = parseGraph(nodeInputList);
 
-			List<string> paths = getPaths(graph, "start", "");
+			Func<string,string,bool> visitMethod = canVisit;
+			List<string> paths = getPaths(graph, "start", "", canVisit);
 
 			Console.WriteLine("Path Count: " + paths.Count);
 		}
@@ -19,12 +20,49 @@ namespace aoc2021
 		{
 			Console.WriteLine("Day 12 - Part Two");
 
+			string[] nodeInputList = InputReader.ReadFileAsStrings("input-d12.txt");
+
+			Dictionary<string,List<string>> graph = parseGraph(nodeInputList);
+
+			HashSet<string> paths = new HashSet<string>();
+			getPaths2(graph, "start", "", paths);
+
+			Console.WriteLine("Paths: " + paths.Count);
 		}
 
-		private List<string> getPaths(Dictionary<string,List<string>> graph, string start, string currentPath)
+		private void getPaths2(Dictionary<string,List<string>> graph, string start, string currentPath, HashSet<string> allPaths)
+		{
+			if (start == "end")
+			{
+				allPaths.Add(currentPath + "." + start);
+				return;
+			}	
+
+			if (graph.ContainsKey(start))
+			{
+				foreach(string nextNode in graph[start])
+				{
+					if (canVisitPartTwo(currentPath + "." + start, nextNode))
+					{
+						getPaths2(graph,nextNode, currentPath + "." + start,allPaths);
+					}
+				}
+			}
+			else
+			{
+
+			}
+		}
+
+
+		private List<string> getPaths(Dictionary<string,List<string>> graph, string start, string currentPath, Func<string,string,bool> canVisit)
 		{
 			List<string> paths = new List<string>();
 
+			if (currentPath.Contains(".end"))
+			{
+				return paths;
+			}
 			string delim  = "";
 			if (currentPath != "")
 			{
@@ -32,6 +70,7 @@ namespace aoc2021
 			}
 
 			string newPath = currentPath + delim + start;
+
 			if (graph.ContainsKey(start))
 			{
 				foreach(string nextNode in graph[start])
@@ -42,7 +81,7 @@ namespace aoc2021
 						continue;
 					}
 
-					List<string> nextPaths = getPaths(graph, nextNode, newPath);
+					List<string> nextPaths = getPaths(graph, nextNode, newPath, canVisit);
 
 					foreach(string nextPath in nextPaths)
 					{
@@ -74,8 +113,61 @@ namespace aoc2021
 
 			return true;
 		}
+
+		private bool canVisitPartTwo(string path, string node)
+		{
+			bool smallCave = node.Equals(node.ToLower());
+
+			if (smallCave)
+			{
+				if (path.Contains(node))
+				{
+					if (hasTwoCaves(path))
+					{
+						return false;
+					}
+
+					if (node == "start" || node == "end")
+					{
+						return false;
+					}
+				}
+							
+			}
+			return true;
+		}
+
+		private bool hasTwoCaves(string path)
+		{
+			Dictionary<string,int> caveCount = new Dictionary<string,int>();
+			string[] caves = path.Split(".");
+			foreach(string cave in caves)
+			{
+				if (cave.ToUpper() == cave)
+				{
+					continue;
+				}
+				if (!caveCount.ContainsKey(cave))
+				{
+					caveCount[cave] = 0;
+				}
+				caveCount[cave] = caveCount[cave] + 1;
+			}
+			bool doubleCave = false;
+
+			foreach(KeyValuePair<string,int> kvp in caveCount)
+			{
+				if (kvp.Value >1)
+				{
+					doubleCave = true;
+					break;
+				}
+			}
+			return doubleCave;
+		}
 		private void addPath(List<string> paths, string newPath)
 		{
+
 			if (paths.Contains(newPath))
 			{
 				return;
