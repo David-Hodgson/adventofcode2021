6251e8a08b16c8187005ce2bb3d8cc7d92ea8d5d
David Hodgson <dahodgson@gmail.com>
2021-12-27 12:51:59 +0000

    Day Sixteen - Tempalte

---
+++ DaySixteen.cs
@@ -0,0 +1,18 @@
+namespace aoc2021
+{
+	public class DaySixteen{
+
+		public void PartOne()
+		{
+			Console.WriteLine("Day 16 - Part One");
+
+			string input = InputReader.ReadFileAsString("input-d16.txt");
+		}
+
+		public void PartTwo()
+		{
+			Console.WriteLine("Day 16 - Part Two");
+
+		}
+	}
+}
